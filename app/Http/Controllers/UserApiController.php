37f19d0efd2b276563a86786e6abfb5dcd0bd28a
s-kim-sysnavi <s-kim@sysnavi.co.jp>
2025-02-18 17:43:36 +0900

    リクエストによって、取得できるカラムが増えるように修正

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -8,37 +8,66 @@ use App\Models\User;
 class UserApiController extends Controller
 {
     //
-    public function index_all()
+    // public function index_all()
+    // {
+    //     $users = User::all();
+    //     return response()->json($users);
+    // }
+
+    // public function index_limit()
+    // {
+    //     $users = User::all()->map(function ($user) {
+    //         return $user->only(["id", "last_name", "first_name", "last_name_kana", "first_name_kana", "email"]);
+    //     });
+
+    //     return response()->json($users);
+    // }
+    public function index(Request $request)
     {
-        $users = User::all();
+        $fields = explode(',', $request->query('fields', 'id,last_name,first_name,last_name_kana,first_name_kana,email,created_at,updated_at'));
+        $allowedFields = ['id', 'last_name', 'first_name', 'email', 'gender', 'birth_date', 'join_date', 'post_code', 'address1', 'address2', 'address3', 'created_at', 'updated_at'];
+
+        $fields = array_intersect($fields, $allowedFields);
+
+        if (empty($fields)) {
+            return response()->json(['message' => '該当する情報が存在しません。'], 400);
+        }
+
+        $users = User::select($fields)->get();
         return response()->json($users);
     }
 
-    public function index_limit()
+    // public function show(user $user)
+    // {
+    //     $user = User::find($user->id)->only(["id", "last_name", "first_name", "last_name_kana", "first_name_kana", "email"]);
+
+    //     if (!$user) {
+    //         return response()->json(['message' => '存在していない社員です'], 404);
+    //     }
+    //     return response()->json($user);
+    // }
+
+    // public function show_all(user $user)
+    // {
+    //     $user = User::find($user->id);
+    //     if (!$user) {
+    //         return response()->json(['message' => '存在していない社員です'], 404);
+    //     }
+    //     return response()->json($user);
+    // }
+
+    public function show(user $user, Request $request)
     {
-        $users = User::all()->map(function ($user) {
-            return $user->only(["id", "last_name", "first_name", "last_name_kana", "first_name_kana", "email"]);
-        });
+        $fields = explode(',', $request->query('fields', 'id,last_name,first_name,last_name_kana,first_name_kana,email,created_at,updated_at'));
+        $allowedFields = ['id', 'last_name', 'first_name', 'email', 'gender', 'birth_date', 'join_date', 'post_code', 'address1', 'address2', 'address3', 'created_at', 'updated_at'];
 
-        return response()->json($users);
-    }
+        $fields = array_intersect($fields, $allowedFields);
 
-    public function show(user $user)
-    {
-        $user = User::find($user->id)->only(["id", "last_name", "first_name", "last_name_kana", "first_name_kana", "email"]);
-
-        if (!$user) {
-            return response()->json(['message' => '存在していない社員です'], 404);
+        if (empty($fields)) {
+            return response()->json(['message' => '該当する情報が存在しません。'], 400);
         }
-        return response()->json($user);
-    }
+        $userData = $user->only($fields);
 
-    public function show_all(user $user)
-    {
-        $user = User::find($user->id);
-        if (!$user) {
-            return response()->json(['message' => '存在していない社員です'], 404);
-        }
-        return response()->json($user);
+        return response()->json($userData);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user-info/all-full', [UserApiController::class, 'index_all'])->name('user_info.all_full');
+// Route::get('/user-info/all-full', [UserApiController::class, 'index_all'])->name('user_info.all_full');
 
-Route::get('/user-info/all', [UserApiController::class, 'index_limit'])->name('user_info.all');
+// Route::get('/user-info/all', [UserApiController::class, 'index_limit'])->name('user_info.all');
+
+Route::get('/user-info/all', [UserApiController::class, 'index'])->name('user_info.all');
 
 Route::get('/user-info/{user}', [UserApiController::class, 'show'])->name('user_info.detail');
 
-Route::get('/user-info/{user}/full', [UserApiController::class, 'show_all'])->name('user_info.detail_full');
+// Route::get('/user-info/{user}/full', [UserApiController::class, 'show_all'])->name('user_info.detail_full');
